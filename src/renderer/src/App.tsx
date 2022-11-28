25dee133f47d7c6e1d2d7399775ddd13db65ca4f
gregory6z <gregoryrag@gmail.com>
2022-11-28 20:36:26 +0100

    initial

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "styled-components"
+import { PdfProvider } from "./context"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+import { Worker } from "@react-pdf-viewer/core"
+import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom"
+import "./lib/pdf.worker"
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <PdfProvider>
+        <Worker workerUrl="./lib/pdf.worker">
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </Worker>
+      </PdfProvider>
+    </ThemeProvider>
+  )
+}
